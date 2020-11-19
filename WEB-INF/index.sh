15c0fc1ee5bb978430ba38acee2d95dd790a1ef0
Frédéric Glorieux <frederic.glorieux@fictif.org>
2020-11-19 03:01:10 +0100

    Encore en cours

--- WEB-INF/index.sh
+++ WEB-INF/index.sh
@@ -1,3 +1,3 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-java -cp $DIR/lib/ddrlab.jar ddrlab.Base "$@"
-touch $DIR/web.xml # reload webapp
+java -cp "$DIR/lib/alix.jar:$DIR/lib/lucene-core-8.7.0.jar:$DIR/ext/saxon9.jar" alix.cli.Load "$@"
+# touch $DIR/web.xml # reload webapp
